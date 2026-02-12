b46304e1cc32a81f53f68e3782d13d8989a97ff5
karolyia79 <87782660+karolyia79@users.noreply.github.com>
2026-02-12 21:34:30 +0100

    Create config_flow.py

---
+++ custom_components/smart_grid_yield/config_flow.py
@@ -0,0 +1,40 @@
+import voluptuous as vol
+from homeassistant import config_entries
+from homeassistant.helpers import selector
+from .const import (
+    DOMAIN, CONF_SPOT_PRICE, CONF_LOAD_POWER, CONF_GRID_POWER, 
+    CONF_FIXER_API_KEY, CONF_DAILY_IMPORT, CONF_DAILY_EXPORT
+)
+
+class SGYFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        if "nordpool" not in self.hass.config.components:
+            return self.async_abort(reason="missing_nordpool")
+        
+        if user_input is not None:
+            return self.async_create_entry(title="SGY Simulator", data=user_input)
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema({
+                vol.Required(CONF_FIXER_API_KEY): str,
+                vol.Required(CONF_SPOT_PRICE): selector.EntitySelector(
+                    selector.EntitySelectorConfig(domain="sensor")
+                ),
+                vol.Required(CONF_LOAD_POWER): selector.EntitySelector(
+                    selector.EntitySelectorConfig(domain="sensor")
+                ),
+                vol.Required(CONF_GRID_POWER): selector.EntitySelector(
+                    selector.EntitySelectorConfig(domain="sensor")
+                ),
+                # Új mezők a napi kWh adatokhoz
+                vol.Required(CONF_DAILY_IMPORT): selector.EntitySelector(
+                    selector.EntitySelectorConfig(domain="sensor")
+                ),
+                vol.Required(CONF_DAILY_EXPORT): selector.EntitySelector(
+                    selector.EntitySelectorConfig(domain="sensor")
+                ),
+            })
+        )
