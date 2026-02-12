90a4c9dcb19dd651f4f2d81297a1118785a9cf43
karolyia79 <87782660+karolyia79@users.noreply.github.com>
2026-02-12 19:57:27 +0100

    Create const.py

---
+++ custom_components/smart_grid_yield/const.py
@@ -0,0 +1,6 @@
+DOMAIN = "smart_grid_yield"
+CONF_SPOT_PRICE = "spot_price_sensor"
+CONF_LOAD_POWER = "load_power_sensor"
+CONF_GRID_POWER = "grid_power_sensor"
+CONF_FIXER_API_KEY = "fixer_api_key"
+PLATFORMS = ["sensor"]
