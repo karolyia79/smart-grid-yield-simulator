b3f987960371584aca9cfd104c3db6235a5e9bb8
karolyia79 <87782660+karolyia79@users.noreply.github.com>
2026-02-13 00:43:56 +0100

    Update __init__.py

--- custom_components/smart_grid_yield/__init__.py
+++ custom_components/smart_grid_yield/__init__.py
@@ -46,7 +46,4 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
-    if unload_ok:
-        hass.data[DOMAIN].pop(entry.entry_id)
-    return unload_ok
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
