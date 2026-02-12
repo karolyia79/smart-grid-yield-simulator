7c8bef9cddb0181be9ea0c16ab7f382998d544ec
karolyia79 <87782660+karolyia79@users.noreply.github.com>
2026-02-13 00:33:43 +0100

    Update const.py

--- custom_components/smart_grid_yield/const.py
+++ custom_components/smart_grid_yield/const.py
@@ -1,14 +1,15 @@
 DOMAIN = "smart_grid_yield"
 
-# Konfigurációs típusok
 CONF_PHASE_SETTING = "phase_setting"
 PHASE_1 = "1_phase"
 PHASE_3_AGGREGATED = "3_phase_aggregated"
 PHASE_3_INDIVIDUAL = "3_phase_individual"
 
-# Szenzor kulcsok
 CONF_SPOT_PRICE = "spot_price_sensor"
 CONF_LOAD_POWER = "load_power_sensor"
+CONF_PV_POWER = "pv_power_sensor"
+CONF_BATT_CHARGE = "batt_charge_sensor"
+CONF_BATT_DISCHARGE = "batt_discharge_sensor"
 CONF_GRID_POWER = "grid_power_sensor"
 CONF_GRID_L1 = "grid_l1_sensor"
 CONF_GRID_L2 = "grid_l2_sensor"
