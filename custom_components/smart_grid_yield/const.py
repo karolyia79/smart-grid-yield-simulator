4bb7e66d138b6e4a7e194c0f3ffbdde785dcfc54
karolyia79 <87782660+karolyia79@users.noreply.github.com>
2026-02-12 21:43:35 +0100

    Update const.py

--- custom_components/smart_grid_yield/const.py
+++ custom_components/smart_grid_yield/const.py
@@ -3,9 +3,9 @@ CONF_SPOT_PRICE = "spot_price_sensor"
 CONF_LOAD_POWER = "load_power_sensor"
 CONF_GRID_POWER = "grid_power_sensor"
 CONF_FIXER_API_KEY = "fixer_api_key"
-
-# Új entitás kulcsok a napi kWh adatokhoz
 CONF_DAILY_IMPORT = "daily_import_sensor"
 CONF_DAILY_EXPORT = "daily_export_sensor"
+CONF_BATTERY_CAPACITY = "battery_capacity"
+CONF_BATTERY_RESERVE = "battery_reserve"
 
 PLATFORMS = ["sensor"]
